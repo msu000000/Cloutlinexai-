e007f34e2d53352418858f30496533458feca491
msu000000 <msuvi000@gmail.com>
2025-08-03 11:56:22 +0530

    Create Footer.js

---
+++ pages/pages/components/components/Footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="bg-black text-gray-400 text-center py-4 mt-10">
+      <p>© {new Date().getFullYear()} Cloutline AI. All rights reserved.</p>
+    </footer>
+  );
+}
